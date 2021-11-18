8796af608a14ffa2dff7702c06432ebaee1e8c1f
Abu Baker Kahtan <abumkahtan@gmail.com>
2021-11-18 14:18:10 +0000

    New switch statement and updates to text and overall look.

--- src/com/tsi/training/kahtan/abubakr/paintercalc/PainterCalc.java
+++ src/com/tsi/training/kahtan/abubakr/paintercalc/PainterCalc.java
@@ -1,51 +1,68 @@
 package com.tsi.training.kahtan.abubakr.paintercalc;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class PainterCalc {
 
         public static void main(String[] args){
             Scanner scan = new Scanner(System.in);
-            int painterInput;
-            int wallCoverage[];
+            int wallCoverage = 0; //This is an array of all the wall sizes in Sq-ft
+            int coveragePerHalfL = 52;   // How much Sq-ft is covered by a 500mL Can of paint
             int numberOfWalls;
-            int coveragePerHalfCan = 52;   // How much Sqft is covered by a 500mL Can of paint
             int mlPerCan;
             int numOfCans;
             int coveragePerCan;
             int i = 0;
+            String userIn;
 
-            //System.out.println("What is the Sqft measurement of coverage you need?: ");
-            //painterInput = scan.nextInt();       // Value to be replaced by scanner
-            System.out.println("How many walls would you like to paint?:");
-            numberOfWalls = scan.nextInt();
-            while (i < numberOfWalls) {
 
-                int wallLength;
-                int wallHeight;
-                
+            //Switch statement here to enter the whole calculator?
+            System.out.println("Would you like to use the Painter Calculator? (y/n):");
+            userIn = scan.nextLine();
 
-                System.out.println("what is the height of the wall?:");
-                wallHeight = scan.nextInt();
-                System.out.println("what is the length of the wall?:");
-                wallLength = scan.nextInt();
+            switch (userIn) {
+                case "y":
 
+                    System.out.println("How many walls would you like to paint?:");
+                    numberOfWalls = scan.nextInt();
+
+                    System.out.println("How many ml is in per can you will be using?:");
+                    mlPerCan = scan.nextInt();
+
+                    while (i < numberOfWalls) {
+
+                        int wallLength = 0;
+                        int wallHeight = 0;
+                        i++;
+
+                        System.out.println("what is the height in ft of wall number " + i + " ?:");
+                        wallHeight = scan.nextInt();
+
+                        System.out.println("what is the length in ft of wall number " + i + "?:");
+                        wallLength = scan.nextInt();
+
+                        wallCoverage = wallCoverage + (wallHeight*wallLength); //Adds the Sq-ft per wall onto the current total.
+                    }
+
+                    coveragePerCan = (int)(coveragePerHalfL*(mlPerCan/500));
+                    numOfCans = wallCoverage / coveragePerCan;
+
+                    System.out.println("The total Sq-ft you need to paint is: " + wallCoverage);
+
+                    if(wallCoverage%coveragePerCan > 0){
+                        numOfCans++;
+                        System.out.println("The number of chosen size cans the painter needs for this coverage is: " + numOfCans + ", and you will have leftover paint");
+                    }
+                    else {
+                        System.out.println("The number of chosen size cans the painter needs for this coverage is: " + numOfCans);
+                    }
+
+                    break;
+                case "n":
+                    System.out.println("We will now stop the program!");
+                    break;
             }
 
-            System.out.println("How many ml is in per can you will be using?:");
-            mlPerCan = scan.nextInt();
-
-            painterInput =
-            coveragePerCan = (int)(coveragePerHalfCan*(mlPerCan/500));
-            numOfCans = painterInput / coveragePerCan;
-
-
-            if(painterInput%coveragePerCan > 0){
-                numOfCans++;
-                System.out.println("The number of 1L cans the painter needs for this coverage is: " + numOfCans + ", and you will have leftover paint");
-            }
-            else{
-                System.out.println("The number of 1L cans the painter needs for this coverage is: " + numOfCans);
-            }
        }
 }
